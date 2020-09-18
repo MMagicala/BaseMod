2b746f96624fff716789535054bc8f907de19b6f
MMagicala <36617521+MMagicala@users.noreply.github.com>
2020-09-18 16:47:55 -0700

    wip 3

--- mod/src/main/java/basemod/ModNumberWidget.java
+++ mod/src/main/java/basemod/ModNumberWidget.java
@@ -1,11 +1,13 @@
 package basemod;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
+import basemod.ModLabel;
+import basemod.ModLabeledButton;
+import basemod.ModPanel;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.Hitbox;
-import org.w3c.dom.ranges.RangeException;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
@@ -13,7 +15,7 @@ public class ModNumberWidget {
     private static final int UI_PADDING = 10;
     private static final int LABEL_SPACE_WIDTH = 50;
     private int lowestOffset;
-    private ArrayList<ModLabeledButton> btns;
+    private ModLabeledButton[] btns;
 
     private Hitbox hb;
 
@@ -24,56 +26,50 @@ public class ModNumberWidget {
         if (value < 0) {
             throw new Exception("Value cannot be below zero");
         }
-        if(numBtns % 2 != 0){
+        if (numBtns % 2 != 0) {
             throw new Exception("Must have even number of buttons");
         }
 
-        btns = new ArrayList<>();
+        btns = new ModLabeledButton[numBtns];
 
         // Create hitbox for the widget
         // TODO: Fix btn height
-        hb = new Hitbox(x, y, UI_PADDING*3+LABEL_SPACE_WIDTH, 50);
+        hb = new Hitbox(x, y, UI_PADDING * 3 + LABEL_SPACE_WIDTH, 50);
 
         // Create center label
-        ModLabel centerLabel = new ModLabel(Integer.toString(value), x + UI_PADDING + LABEL_SPACE_WIDTH/2f, y,
-                FontHelper.charTitleFont, modPanel, me -> {});
+        ModLabel centerLabel = new ModLabel(Integer.toString(value), x + UI_PADDING + LABEL_SPACE_WIDTH / 2f, y,
+                FontHelper.charTitleFont, modPanel, me -> {
+            // Use the label update function to check for mouse wheel input on the widget
+            if(hb.hovered) {
+                if (Gdx.input.isButtonPressed(Input.Buttons.FORWARD)) {
+                    changeOffset(false);
+                }
+                if (Gdx.input.isButtonPressed(Input.Buttons.BACK)) {
+                    changeOffset(true);
+                }
+            }
+        });
         modPanel.addUIElement(centerLabel);
 
-        // Create btns
-
+        // Determine lowest offset
         int digitCount = Integer.toString(value).length();
         lowestOffset = (int) Math.pow(10, digitCount - 1);
 
-        int[] deltas = new int[numBtns];
-        // Negative btns
-        for(int i = 0; i < numBtns/2; i++){
-            deltas[i] = ;
-        }
-
-        // Positive btns
-        for(int i = 0; i < numBtns/2; i++){
-            deltas[i] = ;
-        }
-
-        // {-10 * lowestOffset, -lowestOffset, lowestOffset, 10 * lowestOffset};
-
-        for (int i = 0; i < 4; i++) {
-            StringBuilder sb = new StringBuilder();
-            if (deltas[i] > 0) {
-                sb.append('+');
-            }
-            sb.append(deltas[i]);
+        // Create btns
+        for (int i = 0; i < numBtns; i++) {
             int xPos = x + i * UI_PADDING;
+            // Account for label
             if (i > 1) {
                 xPos += LABEL_SPACE_WIDTH;
             }
             int index = i;
-            ModLabeledButton deltaBtn = new ModLabeledButton(sb.toString(), xPos, y, modPanel, (btn) -> {
+
+            ModLabeledButton deltaBtn = new ModLabeledButton("", xPos, y, modPanel, (btn) -> {
                 if (value > 0) {
                     int v = value;
-                    v += deltas[index];
+                    v += Integer.parseInt(btns[index].label);
                     // Value cannot be below zero
-                    if(v < 0){
+                    if (v < 0) {
                         v = 0;
                     }
 
@@ -81,30 +77,33 @@ public class ModNumberWidget {
                     centerLabel.text = Integer.toString(v);
 
                     // TODO: Run defined code
-
                 }
             }
             );
             modPanel.addUIElement(deltaBtn);
-            btns.add(deltaBtn);
         }
-
         applyOffset();
     }
 
     // false = decrease offset
-    private void changeOffset(boolean increaseOffset){
-        int[] deltas = {-10 * lowestOffset, -lowestOffset, lowestOffset, 10 * lowestOffset};
-        StringBuilder sb = new StringBuilder();
-        if (deltas[i] > 0) {
-            sb.append('+');
-        }
-        sb.append(deltas[i]);
-
+    private void changeOffset(boolean increaseOffset) {
         if(increaseOffset){
+            lowestOffset *= 10;
+        }else if(lowestOffset >= 10){
+            lowestOffset /= 10;
+        }
+        applyOffset();
+    }
 
-        }else{
-
+    private void applyOffset(){
+        int[] deltas = {-10 * lowestOffset, -lowestOffset, lowestOffset, 10 * lowestOffset};
+        for (int i = 0; i < 4; i++) {
+            StringBuilder sb = new StringBuilder();
+            if (deltas[i] > 0) {
+                sb.append('+');
+            }
+            sb.append(deltas[i]);
+            btns[i].label = sb.toString();
         }
     }
 }
